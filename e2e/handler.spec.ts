dd4557b14283bf8311464144f32dd259ae3f2eef
tseijp <_@tsei.jp>
2024-12-13 17:37:50 +0900

    fix: sqlite handler (#2)

--- e2e/handler.spec.ts
+++ e2e/handler.spec.ts
@@ -1,49 +1,55 @@
-import { test, expect } from '@playwright/test';
+import { routes } from "@/handler";
+import { test, expect } from "@playwright/test";
+import { testClient } from "hono/testing";
 
-const url = "http://localhost:3000";
-const iso = new Date().toISOString()
-const id = 1
+const app = testClient(routes);
+const iso = new Date().toISOString();
+const id = "1";
 
-test("Initialize the database", async ({ request }) => {
-  const res = await request.post(`${url}/init`);
-  expect(res.ok()).toBeTruthy();
+test.beforeAll("Initialize the database", async () => {
+  const res = await app.init.$get();
+  expect(res.ok).toBeTruthy();
 });
 
-test("insert a new alert message", async ({ request }) => {
-  const data = { title: "a", message: "b", created_by: "c" };
-  const res = await request.post(url, { data });
-  expect(res.ok()).toBeTruthy();
+test("List all data", async () => {
+  const res = await app.index.$get();
+  expect(res.ok).toBeTruthy();
 });
 
-test("Update display times for an alert message", async ({ request }) => {
-  // Assuming 'id' is known from a previous test or setup
-  const data = { display_start: iso, display_end: iso };
-  const response = await request.patch(`${url}/msgs/${id}/display`, { data });
-  expect(response.ok()).toBeTruthy();
+test("Insert a new message", async () => {
+  const json = { title: "title", content: "content" };
+  const res = await app.index.$post({ json });
+  expect(res.ok).toBeTruthy();
 });
 
-test("Update alert message title and content", async ({ request }) => {
-  const data = { title: "Updated Title", content: "Updated content" };
-  const res = await request.patch(`${url}/msgs/${id}`, { data });
-  expect(res.ok()).toBeTruthy();
+test("Update message title and content", async () => {
+  const json = { title: "title", content: "content" };
+  const res = await app[":id"].$patch({ param: { id }, json });
+  expect(res.ok).toBeTruthy();
 });
 
-test("Update target stage of an alert message", async ({ request }) => {
-  const data = { target_stage: "release" }; // Changing to release stage
-  const res = await request.patch(`${url}/msgs/${id}`, { data });
-  expect(res.ok()).toBeTruthy();
+test("Update display times for an message", async () => {
+  const json = { display_start: iso, display_end: iso };
+  const res = await app[":id"].display.$patch({ param: { id }, json });
+  expect(res.ok).toBeTruthy();
 });
 
-test("Logically delete an alert message", async ({ request }) => {
-  const res = await request.delete(`${url}/msgs/${id}`);
-  expect(res.ok()).toBeTruthy();
+test("Update target stage of an message", async () => {
+  const json = { target_stage: "release" };
+  const res = await app[":id"].target.$patch({ param: { id }, json });
+  expect(res.ok).toBeTruthy();
+});
+
+test("Logically delete an message", async () => {
+  const res = await app[":id"].$delete({ param: { id } });
+  expect(res.ok).toBeTruthy();
 });
 
 const stages = ["develop", "release", "product"];
 
 stages.forEach((stage) => {
-  test(`Retrieve alert message for ${stage} stage`, async ({ request }) => {
-    const res = await request.get(`${url}/msgs/${stage}`);
-    expect(res.ok()).toBeTruthy();
+  test(`Retrieve message for ${stage} stage`, async () => {
+    const res = await app[":stage"].$get({ param: { stage } });
+    expect(res.ok).toBeTruthy();
   });
 });
--- handler.ts
+++ handler.ts
@@ -1,14 +1,19 @@
 // handler.ts
 import { serve } from "@hono/node-server";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/aws-lambda";
 import sqlite3 from "sqlite3";
+import { z } from "zod";
 
-const EFS_PATH = "./db.sqlite3";
+
+const isLocal = process.env.RUNTIME_ENV === "local";
+const isServe = process.env.RUNTIME_ENV === "serve";
+const DB_PATH = isLocal || isServe ? "./db.sqlite3" : "/mnt/db/db.sqlite";
 
 const app = new Hono();
 
-const db = new sqlite3.Database(EFS_PATH);
+const db = new sqlite3.Database(DB_PATH);
 
 export async function all<T>(query: string, ...args: any[]) {
   return new Promise<T>((resolve, reject) => {
@@ -47,90 +52,130 @@ interface Item {
   target_stage: string;
 }
 
+const tableCreationQuery = /* SQL */ `
+CREATE TABLE IF NOT EXISTS items (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  content TEXT,
+  display_start DATETIME,
+  display_end DATETIME,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  deleted_at DATETIME,
+  created_by TEXT,
+  target_stage TEXT DEFAULT 'develop'
+);
+`.trim();
+
+const createSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+const updateSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+const updateAuthorSchema = z.object({
+  created_by: z.string(),
+});
+
+const updateDisplaySchema = z.object({
+  display_start: z.string(),
+  display_end: z.string(),
+});
+
+const updateTargetSchema = z.object({
+  target_stage: z.string(),
+});
+
 // Route to initialize the database
-app.get("/init", async (c) => {
-  await run(`DROP TABLE IF EXISTS msgs;`);
-  const q = /* SQL */ `
-    CREATE TABLE msgs (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT,
-      content TEXT,
-      display_start DATETIME,
-      display_end DATETIME,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      deleted_at DATETIME,
-      created_by TEXT,
-      target_stage TEXT DEFAULT 'develop'
-    );
-  `;
-  await run(q);
-  return c.json({ message: "initialized" });
-});
+export const routes = app
+  .get("/init", async (c) => {
+    await run(tableCreationQuery);
+    return c.json({ message: "inited" });
+  })
+  // @TODO REMOVE
+  .get("/drop", async (c) => {
+    await run(/* SQL */ `DROP TABLE IF EXISTS items;`);
+    return c.json({ message: "droped" });
+  })
+  // get to fetch paginated alert messages
+  .get("/", async (c) => {
+    const page = parseInt(c.req.query("page") || "1");
+    const limit = 10;
+    const q = /* SQL */ `SELECT * FROM items ORDER BY created_at DESC LIMIT ? OFFSET ?`;
+    const res = await all<Item[]>(q, limit, limit * (page - 1));
+    return c.json(res);
+  })
+  // insert a new alert message
+  .post("/", zValidator("json", createSchema), async (c) => {
+    const { title, content } = c.req.valid("json");
+    const q = /* SQL */ `INSERT INTO items (title, content) VALUES (?, ?)`;
+    const id = await run(q, title, content);
+    return c.json({ id }, 201);
+  })
+  // update the alert message and title
+  .patch("/:id", zValidator("json", updateSchema), async (c) => {
+    const { title, content } = await c.req.json();
+    const id = c.req.param("id");
+    const q = /* SQL */ `UPDATE items SET title = ?, content = ? WHERE id = ?`;
+    await run(q, title, content, id);
+    return c.json({ message: "updated" });
+  })
+  // delete for logical deletion of an alert
+  .delete("/:id", async (c) => {
+    const id = c.req.param("id");
+    const q = /* SQL */ `UPDATE items SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?`;
+    await run(q, id);
+    return c.json({ message: "deleted" });
+  })
+  // update author
+  .patch("/:id/author", zValidator("json", updateAuthorSchema), async (c) => {
+    const { created_by } = c.req.valid("json");
+    const id = c.req.param("id");
+    const q = /* SQL */ `UPDATE items SET created_by = ? WHERE id = ?`;
+    await run(q, created_by, id);
+    return c.json({ message: "updated" });
+  })
+  // update target stage
+  .patch("/:id/target", zValidator("json", updateTargetSchema), async (c) => {
+    const { target_stage } = c.req.valid("json");
+    const id = c.req.param("id");
+    const q = /* SQL */ `UPDATE items SET target_stage = ? WHERE id = ?`;
+    await run(q, target_stage, id);
+    return c.json({ message: "updated" });
+  })
+  // update display start time
+  .patch("/:id/display", zValidator("json", updateDisplaySchema), async (c) => {
+    const { display_start, display_end } = c.req.valid("json");
+    const id = c.req.param("id");
+    const q = /* SQL */ `UPDATE items SET display_start = ?, display_end = ? WHERE id = ?`;
+    await run(q, display_start, display_end, id);
+    return c.json({ message: "updated" });
+  })
+  // get to display the most current alert message based on stage
+  .get("/:stage", async (c) => {
+    const stage = c.req.param("stage");
+    const query = /* SQL */ `
+      SELECT title, content FROM items
+      WHERE target_stage = ?
+      AND deleted_at IS NULL
+      AND (display_start IS NULL OR display_start <= CURRENT_TIMESTAMP)
+      AND (display_end IS NULL OR display_end >= CURRENT_TIMESTAMP)
+      ORDER BY created_at DESC
+      LIMIT 1;
+    `;
+    const res = await one<{ title: string; content: string }>(query, stage);
+    return c.json(res);
+  });
 
-// Route to fetch paginated alert messages
-app.get("/", async (c) => {
-  const page = parseInt(c.req.query("page") || "1");
-  const limit = parseInt(c.req.query("page") || "25");
-  const offset = (page - 1) * limit;
-  const q = `SELECT * FROM msgs ORDER BY created_at DESC LIMIT ? OFFSET ?`;
-  const results = await all<Item[]>(q, limit, offset);
-  return c.json(results);
-});
-
-// Route to insert a new alert message
-app.post("/", async (c) => {
-  const { title, message } = await c.req.json();
-  const q = "INSERT INTO msgs (title, message) VALUES (?, ?)";
-  const id = await run(q, title, message);
-  return c.json({ id }, 201);
-});
-
-// Route to display the most current alert message based on stage
-app.get("/msgs/:stage", async (c) => {
-  const stage = c.req.param("stage");
-  const q = `
-    SELECT title, content FROM msgs
-    WHERE target_stage = ?
-    AND deleted_at IS NULL
-    AND (display_start IS NULL OR display_start <= CURRENT_TIMESTAMP)
-    AND (display_end IS NULL OR display_end >= CURRENT_TIMESTAMP)
-    ORDER BY created_at DESC
-    LIMIT 1;
-  `;
-  const result = await one<{ title: string; content: string }>(q, stage);
-  return c.json(result);
-});
-
-// Route to update the alert message and title
-app.patch("/msgs/:id", async (c) => {
-  const { title, content, created_by } = await c.req.json();
-  const id = c.req.param("id");
-  const q = "UPDATE msgs SET title = ?, content = ?, created_by = ? WHERE id = ?";
-  await run(q, title, content, created_by, id);
-  return c.json({ message: "updated" });
-});
-
-// Route for logical deletion of an alert
-app.delete("/msgs/:id", async (c) => {
-  const id = c.req.param("id");
-  const q = "UPDATE msgs SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?";
-  await run(q, id);
-  return c.json({ message: "deleted" });
-});
-
-// Route to update display start time
-app.patch("/msgs/:id/display", async (c) => {
-  const { display_start, display_end } = await c.req.json();
-  const id = c.req.param("id");
-  const q = "UPDATE msgs SET display_start = ?, display_end = ? WHERE id = ?";
-  await run(q, display_start, display_end, id);
-  return c.json({ message: "updated" });
-});
-
-if (true) {
-  console.log(`ready: Listening on http://localhost:3000/`);
-  serve({ ...app, port: 3000 });
+if (isServe) {
+  console.log(`ready: Listening on http://localhost:3001/`);
+  serve({ ...app, port: 3001 });
 }
+
+export type AppType = typeof routes;
 
 export const handler = handle(app);

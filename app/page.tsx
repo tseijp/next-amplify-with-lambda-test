1c47ecf68643288290fd9bd089e93726c9210686
tseijp <_@tsei.jp>
2024-12-13 18:43:09 +0900

    feat: add sqlite invoker (#3)

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import { invoker } from "@/invoker";
 import Image from "next/image";
 
+const app = invoker();
+
 export default async function Home() {
+  const res = await app.index.$get();
+  const data = await res.json();
+  const body = JSON.parse((data as any).body);
+
+  // app[":id"].$delete({ param: { id: "1" } });
+
+  // app[":id"].$patch({
+  //   param: { id: "1" },
+  //   json: { title: "title", content: "content" },
+  // });
+
+  // @ts-ignore
+  return data.body;
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
---
+++ invoker.ts
@@ -0,0 +1,39 @@
+/**
+ * ref
+ * https://github.com/honojs/hono/blob/main/src/helper/testing/index.ts
+ * https://developers.cloudflare.com/workers/examples/logging-headers/
+ */
+import { AppType } from "@/handler";
+import { Lambda } from "@aws-sdk/client-lambda";
+import { hc } from "hono/client";
+
+const lambda = new Lambda({
+  region: process.env.VPC_LAMBDA_AWS_REGION!,
+  credentials: {
+    accessKeyId: process.env.VPC_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.VPC_AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+const customFetch = async (path: RequestInfo | URL, init?: RequestInit) => {
+  const { body, method, headers } = init ?? {};
+  const payload = {
+    path,
+    body,
+    httpMethod: method,
+    headers: Object.fromEntries(headers as []),
+    isBase64Encoded: false,
+  };
+
+  const args = {
+    FunctionName: process.env.VPC_LAMBDA_FUNCTION_NAME,
+    Payload: JSON.stringify(payload),
+  };
+
+  const res = await lambda.invoke(args);
+  const buf = Buffer.from(res.Payload!);
+  const data = buf.toString();
+  return new Response(data);
+};
+
+export const invoker = () => hc<AppType>("", { fetch: customFetch });
